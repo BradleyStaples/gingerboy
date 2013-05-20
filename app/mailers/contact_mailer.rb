5237d8643a839c019bcae7c34a53ff15ab7b0f27
Bradley Staples <me@bradleystapl.es>
2013-05-20 16:28:33 -0700

    Making some changes to mailer using separate from/to gmail addresses.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,21 @@
 class ContactMailer < ActionMailer::Base
 
-  default :from => "meekbarbrian@gmail.com"
-  default :to => "the.bradley.staples@gmail.com"
+  # make sure ActionMailer::Base.smtp_settings are set
+  # in config/initializers/setup_mail.rb like so:
+  #
+  #ActionMailer::Base.smtp_settings = {
+  #    :address              => "smtp.gmail.com",
+  #    :port                 => 587,
+  #    :domain               => "gmail.com",
+  #    :user_name            => "email_address@gmail.com",
+  #    :password             => "SuperSneakyPassword",
+  #    :authentication       => :plain,
+  #    :enable_starttls_auto => true
+  #}
+
+
+  default :from =>  "meekbarbarian@gmail.com"
+  default :to =>    "the.bradley.staples@gmail.com"
 
   def form_mail(message)
     @message = message
